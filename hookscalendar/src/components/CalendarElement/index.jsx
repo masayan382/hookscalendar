6f95d2bb20f918057ea0224b7d9e0c729e6272f4
m-takahashi <masan3821031@gmail.com>
2021-02-20 18:20:17 +0900

    calendar整形

--- hookscalendar/src/components/CalendarElement/index.jsx
+++ hookscalendar/src/components/CalendarElement/index.jsx
@@ -2,10 +2,31 @@ import React from "react";
 
 import * as styles from "./style.module.css";
 
-const CalendarElement = ({ children }) => {
+import { Typography } from "@material-ui/core"
+
+import dayjs from "dayjs";
+
+import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar"
+
+const CalendarElement = ({ day }) => {
+    const today = dayjs();
+    const isCurrentMonth = isSameMonth(day, today);
+    const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+    const format = isFirstDay(day) ? "M月D日" : "D";
+    const isToday = isSameDay(day, today);
     return (
         <div className={styles.element}>
-            <div className={styles.date}>{children}</div>
+            <Typography
+                className={styles.date}
+                color={textColor}
+                align="center"
+                variant="caption"
+                component="div"
+            >
+                <span className={isToday ? styles.today : ""}>
+                    {day.format(format)}
+                </span>
+            </Typography>
         </div>
     );
 };
