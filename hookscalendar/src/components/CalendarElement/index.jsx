a97a03457a499804702f1b162829245726ab7805
m-takahashi <masan3821031@gmail.com>
2021-02-21 10:05:28 +0900

    react-redux

--- hookscalendar/src/components/CalendarElement/index.jsx
+++ hookscalendar/src/components/CalendarElement/index.jsx
@@ -10,10 +10,18 @@ import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar"
 
 const CalendarElement = ({ day }) => {
     const today = dayjs();
+
+    // 今月以外をグレーダウン
     const isCurrentMonth = isSameMonth(day, today);
     const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
+    // 文字列のフォーマットをどうするか
+    // 月の最初だけ月情報をつける
     const format = isFirstDay(day) ? "M月D日" : "D";
+
+    // 当日かどうか判断
     const isToday = isSameDay(day, today);
+
     return (
         <div className={styles.element}>
             <Typography
