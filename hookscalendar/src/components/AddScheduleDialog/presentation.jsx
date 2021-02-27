313a875e7abb1b7c16de42fc6a5e4d064252eaa1
m-takahashi <masan3821031@gmail.com>
2021-02-27 11:28:47 +0900

    dialog修正

--- hookscalendar/src/components/AddScheduleDialog/presentation.jsx
+++ hookscalendar/src/components/AddScheduleDialog/presentation.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux"
-import { Dialog, DialogContent } from "@material-ui/core";
+import {
+    Dialog,
+    DialogContent,
+    TextField,
+    DialogActions,
+    Button,
+    Input,
+    Grid
+
+} from "@material-ui/core";
+import { LocationOnOutlined, NotesOutlined } from "@material-ui/icons";
+import { withStyles } from "@material-ui/styles";
 import { addScheduleCloseDialog } from "../../redux/addSchedule/actions";
+
+const spacer = { margin: "4px 0" };
+
+const Title = withStyles({
+    root: { marginBottom: 32, fontSize: 22 }
+})(Input);
 
 const AddScheduleDialog = ({ }) => {
     const isDialogOpen = useSelector(state => state.addSchedule.isDialogOpen);
@@ -11,7 +28,30 @@ const AddScheduleDialog = ({ }) => {
     }
     return (
         <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
-            <DialogContent>dialog</DialogContent>
+            <DialogContent>
+                <Title autoFocus fullWidth placeholder="タイトルと日時を追加" />
+                <Grid container spacing={1} alignItems="center" justify="space-between">
+                    <Grid item>
+                        <LocationOnOutlined />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <TextField style={spacer} fullWidth placeholder="場所を追加" />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={1} alignItems="center" justify="space-between">
+                    <Grid item>
+                        <NotesOutlined />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <TextField style={spacer} fullWidth placeholder="説明を追加" />
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button color="primary" variant="outlined">
+                    保存
+        </Button>
+            </DialogActions>
         </Dialog>
     );
 };
